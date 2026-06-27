177c2518aa9b9edd9122a037b15cf004383b3e05
Achim Ismaili <achim@ismaili.de>
2026-06-27 13:16:24 +0200

    feat(showcase): scaffold astro site with easy-web workspace packages

---
+++ apps/showcase/astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://achimismaili.github.io',
+  base: '/easy-web',
+  output: 'static',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'de'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+});
